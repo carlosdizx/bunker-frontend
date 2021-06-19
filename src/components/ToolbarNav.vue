9a82144cfb1fb4cb5c85996a2ae035edab2c27c6
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-19 11:41:43 -0500

    Creado el toolbar de navegacion

---
+++ src/components/ToolbarNav.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app-bar app color="orange" dark>
+		<v-toolbar-title class="mr-12">Bunker</v-toolbar-title>
+		<v-btn icon>
+			<v-icon>mdi-point-of-sale</v-icon>
+		</v-btn>
+		<v-btn icon>
+			<v-icon>mdi-account</v-icon>
+		</v-btn>
+		<v-btn icon>
+			<v-icon>mdi-food</v-icon>
+		</v-btn>
+    <v-btn icon>
+			<v-icon>mdi-google-analytics</v-icon>
+		</v-btn>
+	</v-app-bar>
+</template>
+
+<script>
+	export default {
+		name: 'ToolbarNav',
+	};
+</script>
+
+<style scoped></style>
