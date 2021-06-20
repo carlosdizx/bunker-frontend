54d7fb1c9ed9daed9d316fde098efc34926c586f
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-20 12:16:02 -0500

    Navegacion dentro de forms para los distintos formularios de registro

--- src/components/form/ToolbarNavForm.vue
+++ src/components/form/ToolbarNavForm.vue
@@ -1,19 +1,38 @@
 <template>
-	<v-app-bar app dark>
-		<template v-slot:extension>
-			<v-spacer></v-spacer>
-			<v-tabs align-with-title>
-				<v-tab>Venta</v-tab>
-				<v-tab>Persona</v-tab>
-				<v-tab>Producto</v-tab>
-			</v-tabs>
-		</template>
-	</v-app-bar>
+	<div>
+		<v-app-bar app dark>
+			<template v-slot:extension>
+				<v-spacer></v-spacer>
+				<v-tabs align-with-title>
+					<v-tab @click="changeIndex(1)">Venta</v-tab>
+					<v-tab @click="changeIndex(2)">Persona</v-tab>
+					<v-tab @click="changeIndex(3)">Producto</v-tab>
+				</v-tabs>
+			</template>
+		</v-app-bar>
+    <SaleRegister v-if="index === 1" />
+    <PersonRegister v-if="index === 2" />
+		<ProductRegister v-if="index === 3" />
+	</div>
 </template>
 
 <script>
+	import PersonRegister from './PersonRegister';
+	import ProductRegister from './ProductRegister';
+	import SaleRegister from './SaleRegister';
 	export default {
 		name: 'ToolbarNavForm',
+		components: {
+			PersonRegister,
+			ProductRegister,
+			SaleRegister,
+		},
+		data: () => ({ index: 1 }),
+		methods: {
+			changeIndex(index) {
+				this.index = index;
+			},
+		},
 	};
 </script>
 
--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,24 +1,15 @@
 <template>
 	<v-app>
 		<ToolbarNavForm />
-		<PersonRegister />
-		<ProductRegister />
-		<SaleRegister />
 	</v-app>
 </template>
 
 <script>
 	import ToolbarNavForm from '../components/form/ToolbarNavForm';
-	import PersonRegister from '../components/form/PersonRegister';
-	import ProductRegister from '../components/form/ProductRegister';
-	import SaleRegister from '../components/form/SaleRegister';
 	export default {
 		name: 'Forms',
 		components: {
 			ToolbarNavForm,
-			PersonRegister,
-			ProductRegister,
-			SaleRegister,
 		},
 	};
 </script>
